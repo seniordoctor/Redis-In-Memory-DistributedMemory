f45006516bbd1b95b8713ddb5f8ec8ac2ad22345
Recep MERT <75563269+seniordoctor@users.noreply.github.com>
2024-08-26 15:43:55 +0300

    Add project files.

---
+++ InMemory.Web/Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public ProductController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+
+        public IActionResult Index()
+        {
+            // 1.yöntem
+            if (String.IsNullOrEmpty(_memoryCache.Get<string>("zaman")))
+            {
+                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            }
+
+            // 2.yöntem, Get yapmaya calisir yoksa Set eder
+            if (!_memoryCache.TryGetValue("zaman", out string zamancache))
+            {
+                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            }
+
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            // Get yapmaya calisir yoksa create eder
+            _memoryCache.GetOrCreate<string>("zaman", entry =>
+            {
+                return DateTime.Now.ToString();
+            });
+
+            ViewBag.zaman = _memoryCache.Get<string>("zaman");
+
+            return View();
+        }
+    }
+}
